2e03479f90cb6ade1c8de01e69c11444ce8ce354
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2019-07-26 19:30:32 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,7 +8,7 @@ def display_board(board)
 end
 
 def valid_move?(board, index)
-  if index < board[index] == " "
+  if index.between?(0,8) && board[index] == " "
     return true
   else
     return false
